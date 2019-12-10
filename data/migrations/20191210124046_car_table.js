fb31de721f1902e5b466762ec03520aa7bfa0bc3
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-10 13:30:18 -0800

    Create seed data with proper fields.

--- data/migrations/20191210124046_car_table.js
+++ data/migrations/20191210124046_car_table.js
@@ -2,21 +2,18 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cars', cars => {
       cars.increment();
-      cars.string('VIN', 255)
+      cars.string('VIN', 17)
       .notNullable()
       .unique()
       .index();
       cars.string('make', 255)
       .notNullable()
-      .unique()
       .index();
       cars.string('model', 255)
       .notNullable()
-      .unique()
       .index();
       cars.integer('mileage')
       .notNullable()
-      .unique()
       .index();
   })
 };
---
+++ data/seeds/cars.js
@@ -0,0 +1,16 @@
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('cars').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cars').insert([
+        { id: 1, VIN: 'KJ4879572HT986757', make: 'TOYOTA', model: 'RAV4', mileage: 90756 },
+        { id: 2, VIN: '46FYT637DJT784JCF', make: 'HONDA', model: 'FIT', mileage: 40345 },
+        { id: 3, VIN: '35D7RH567W7CQ1096', make: 'TOYOTA', model: 'COROLLA', mileage: 60489 },
+        { id: 4, VIN: '1HR6FN69EUH78DUNG', make: 'HONDA', model: 'CIVIC', mileage: 80597 },
+        { id: 5, VIN: '95JF85JE7610FUR08', make: 'DODGE', model: 'SHADOW', mileage: 90756 },
+        { id: 6, VIN: 'JT83O5I279MQ19JB9', make: 'CHRYSLER', model: 'SUNBURST', mileage: 110256 }
+      ]);
+    });
+};
