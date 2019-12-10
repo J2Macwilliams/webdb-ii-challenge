c646f1d2f66a43448ebfd0e647c41108e6f4eb1a
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-10 13:13:52 -0800

    Create migration logic for table cars.

--- data/migrations/20191210124046_car_table.js
+++ data/migrations/20191210124046_car_table.js
@@ -1,8 +1,26 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', cars => {
+      cars.increment();
+      cars.string('VIN', 255)
+      .notNullable()
+      .unique()
+      .index();
+      cars.string('make', 255)
+      .notNullable()
+      .unique()
+      .index();
+      cars.string('model', 255)
+      .notNullable()
+      .unique()
+      .index();
+      cars.integer('mileage')
+      .notNullable()
+      .unique()
+      .index();
+  })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('cars');
 };
